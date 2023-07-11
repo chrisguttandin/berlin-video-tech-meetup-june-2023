12e3108fa8298cd5563be0c3cbd22c270445b4e3
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-07-11 02:28:29 +0200

    fix: add mocked TimingObject for pre-rendering

--- src/app/timing-object.service.ts
+++ src/app/timing-object.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
-import { TimingObject } from 'timing-object';
+import { ITimingObject, TimingObject } from 'timing-object';
 import { TimingProvider } from 'timing-provider';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TimingObjectService {
-    public readonly timingObject = new TimingObject(new TimingProvider('pM47wfJhgXpR3sP8JoyT'));
+    public readonly timingObject: Pick<
+        ITimingObject,
+        'addEventListener' | 'dispatchEvent' | 'query' | 'readyState' | 'removeEventListener' | 'update'
+    >;
+
+    constructor() {
+        try {
+            this.timingObject = new TimingObject(new TimingProvider('pM47wfJhgXpR3sP8JoyT'));
+        } catch {
+            this.timingObject = {
+                // eslint-disable-next-line @typescript-eslint/no-empty-function, no-empty-function
+                addEventListener: () => {},
+                dispatchEvent: () => true,
+                query: () => ({ acceleration: 0, position: 0, timestamp: 0, velocity: 0 }),
+                readyState: 'connecting',
+                // eslint-disable-next-line @typescript-eslint/no-empty-function, no-empty-function
+                removeEventListener: () => {},
+                update: () => Promise.resolve()
+            };
+        }
+    }
 }
