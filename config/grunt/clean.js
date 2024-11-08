e3015b7b0b3d49b8d46503c304f1d5b24c974253
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-11-08 18:53:53 +0100

    build(config): remove clean:runtime task

--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,4 +1,3 @@
 module.exports = {
-    'runtime': ['build/berlin-video-tech-meetup-june-2023/browser/runtime*.js'],
     'source-maps': ['build/berlin-video-tech-meetup-june-2023/browser/**.map']
 };
