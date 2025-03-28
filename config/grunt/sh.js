47f66007063e74f4693224c7fd69a5eb54858c57
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-28 23:02:33 +0100

    build(config): use npm scripts for testing tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -7,23 +7,8 @@ module.exports = () => {
         'build': {
             cmd: 'npx ng build --base-href /berlin-video-tech-meetup-june-2023/ --configuration production --subresource-integrity'
         },
-        'continuous': {
-            cmd: 'npx ng test'
-        },
-        'e2e': {
-            cmd: `npx playwright install --with-deps && \
-                npx playwright test --config config/playwright/config.ts`
-        },
         'rimraf-source-maps': {
             cmd: 'rimraf build/berlin-video-tech-meetup-june-2023/browser/**.map'
-        },
-        'smoke': {
-            cmd: `npx playwright install --with-deps && \
-                IS_SMOKE_TEST=true npx playwright test --config config/playwright/config.ts && \
-                npx hint --telemetry=off https://chrisguttandin.github.io/berlin-video-tech-meetup-june-2023`
-        },
-        'test': {
-            cmd: 'npx ng test --watch false'
         },
         'verify': {
             cmd: `npx bundle-buddy build/berlin-video-tech-meetup-june-2023/browser/*.js.map && \
